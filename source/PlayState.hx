410b0d2872cc7293bfefbd800ea7abfbd542869a
Randomxddd <amongus@sus.com>
2022-02-17 15:52:10 -0300

    fix xd

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,4 +1,4 @@
-package;
+00package;
 
 #if desktop
 import Discord.DiscordClient;
@@ -2938,11 +2938,13 @@ class PlayState extends MusicBeatState
 		
 		
 		if(isDad) {
+			camFocus = 'dad';
 			camFollow.set(dad.getMidpoint().x + 150, dad.getMidpoint().y - 100);
 			camFollow.x += dad.cameraPosition[0];
 			camFollow.y += dad.cameraPosition[1];
 			tweenCamIn();
 		} else {
+			camFocus = 'bf';
 			camFollow.set(boyfriend.getMidpoint().x - 100, boyfriend.getMidpoint().y - 100);
 
 			switch (curStage)
@@ -4244,4 +4246,68 @@ class PlayState extends MusicBeatState
 
 	var curLight:Int = 0;
 	var curLightEvent:Int = 0;
+
+	var camFocus:String = "";
+	var daFunneOffsetMultiplier:Float = 20;
+	var dadPos:Array<Float> = [0, 0];
+	var bfPos:Array<Float> = [0, 0];
+	function triggerCamMovement(num:Float = 0)
+	{
+		if (camFocus == 'bf')
+		{
+			switch (num)
+			{
+				case 2:
+					camFollow.y = bfPos[1] - daFunneOffsetMultiplier;
+					camFollow.x = bfPos[0];
+				case 3:
+					camFollow.x = bfPos[0] + daFunneOffsetMultiplier;
+					camFollow.y = bfPos[1];
+				case 1:
+					camFollow.y = bfPos[1] + daFunneOffsetMultiplier;
+					camFollow.x = bfPos[0];
+				case 0:
+					camFollow.x = bfPos[0] - daFunneOffsetMultiplier;
+					camFollow.y = bfPos[1];
+			}
+		}
+		else
+		{
+			switch (num)
+			{
+				case 2:
+					camFollow.y = dadPos[1] - daFunneOffsetMultiplier;
+					camFollow.x = dadPos[0];
+				case 3:
+					camFollow.x = dadPos[0] + daFunneOffsetMultiplier;
+					camFollow.y = dadPos[1];
+				case 1:
+					camFollow.y = dadPos[1] + daFunneOffsetMultiplier;
+					camFollow.x = dadPos[0];
+				case 0:
+					camFollow.x = dadPos[0] - daFunneOffsetMultiplier;
+					camFollow.y = dadPos[1];
+			}
+		}
+	}
+
+	function getCamOffsets()
+	{
+		dadPos[0] = dad.getMidpoint().x + 150 + dad.cameraPosition[0];
+		dadPos[1] = dad.getMidpoint().y - 100 + dad.cameraPosition[1];
+
+		switch (curStage)
+		{
+			case 'limo':
+				bfPos[0] = boyfriend.getMidpoint().x - 110;
+				bfPos[1] = boyfriend.getMidpoint().y - 95;
+			case 'mall':
+				bfPos[0] = boyfriend.getMidpoint().x - 100;
+				bfPos[1] = boyfriend.getMidpoint().y - 165;
+			case 'school' | 'schoolEvil':
+				bfPos[0] = boyfriend.getMidpoint().x - 290;
+				bfPos[1] = boyfriend.getMidpoint().y - 300;
+			default:
+				bfPos[0] = boyfriend.getMidpoint().x - 100 - boyfriend.cameraPosition[0];
+				bfPos[1] = boyfriend.getMidpoint().y - 100 + boyfriend.cameraPosition[1];
 }
