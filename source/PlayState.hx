b7655413f361f95c56cf258b9c7d900aa75d3685
Randomxddd <amongus@sus.com>
2022-02-18 08:53:12 -0300

    update fix

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3697,10 +3697,9 @@ class PlayState extends MusicBeatState
 			note.wasGoodHit = true;
 			vocals.volume = 1;
 
-			var isSus:Bool = note.isSustainNote; //GET OUT OF MY 
-			HEAD, GET OUT OF MY HEAD, GET OUT OF MY HEAD var 
-			leData:Int = Math.round(Math.abs(note.noteData)); var 
-			leType:String = note.noteType; 
+			var isSus:Bool = note.isSustainNote; //GET OUT OF MY HEAD, GET OUT OF MY HEAD, GET OUT OF MY HEAD 
+			var leData:Int = Math.round(Math.abs(note.noteData)); 
+			var leType:String = note.noteType; 
 //			if (camFocus == 'bf' && ClientPrefs.dynamicCam) 
 //				triggerCamMovement(Math.abs(note.noteData % 4));
 
