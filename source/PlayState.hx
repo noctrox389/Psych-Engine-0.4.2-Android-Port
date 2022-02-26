6323b70140f29ddc8f5845fb5bf5bd99986adbf6
Randomxd <no@gmail>
2022-02-26 18:18:30 -0300

    bruh

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -694,11 +694,7 @@ class PlayState extends MusicBeatState
                 var filesPushed:Array<String> = [];
                 var foldersToCheck:Array<String> = [Paths.getPreloadPath('data/' + Paths.formatToSongPath(SONG.song) + '/')];
 
-                #if MODS_ALLOWED
-                foldersToCheck.insert(0, Paths.mods('data/' + Paths.formatToSongPath(SONG.song) + '/'));
-                if(Paths.currentModDirectory != null && Paths.currentModDirectory.length > 0)
-                        foldersToCheck.insert(0, Paths.mods(Paths.currentModDirectory + '/data/' + Paths.formatToSongPath(SONG.song) + '/'));
-                #end
+              
 
                 for (folder in foldersToCheck)
                 {
